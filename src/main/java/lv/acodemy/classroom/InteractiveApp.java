5a04a71dedf010f5228772822b770df52bdf26b7
dordovska <listovski@inbox.lv>
2022-11-08 20:57:33 +0200

    [master] : Add classroom progress

--- src/main/java/lv/acodemy/classroom/InteractiveApp.java
+++ src/main/java/lv/acodemy/classroom/InteractiveApp.java
@@ -11,25 +11,21 @@ public class InteractiveApp {
         System.out.println("What is your last name?");
         String lastName = chatBot.next();
 
-        System.out.printf("Hello %s %s. My name is bot Alex K. How can I help you?%n" , name, lastName);
+        System.out.printf("Hello %s %s. My name is bot Alex K. How can I help you?%n", name, lastName);
 
         System.out.println("Press1 for registration or press 2 to stop chat");
         byte answer = chatBot.nextByte();
-        if(answer == 1) {
+        if (answer == 1) {
             System.out.println("Please enter a date in following format: (dd-mm-yyyy)");
             String registrationDate = chatBot.next();
             System.out.println("You have registred on date:" + registrationDate);
-        }
-
-            else if (answer 2){
+        } else if (answer == 2) {
             System.out.println("Ok, this chat will be closed now");
             chatBot.close();
         }
 
 
-
-
-            } else {
+    else {
                 System.out.println("Option is not correct");
             }
 
@@ -39,4 +35,3 @@ public class InteractiveApp {
         }
 
     }
-}
---
+++ src/main/java/lv/acodemy/classroom/Variables.java
@@ -0,0 +1,57 @@
+package lv.acodemy.classroom;
+
+public class Variables {
+    public static void main(String[] args) {
+
+
+        /*
+        Hello World!!
+         */
+
+        //byte
+        byte byteVariable = 120;
+        System.out.println(byteVariable);
+
+        //short
+
+        short salary = 2000;
+        System.out.println(salary);
+
+        //int
+        int populationInLatvia = 2000000;
+        System.out.println(populationInLatvia);
+
+        //float
+        float stockPrice = 5000.50f;
+        System.out.println(stockPrice);
+
+        //long
+         long coronovirusCases = 76389745l;
+        System.out.println(coronovirusCases);
+
+        //boolean prefix
+
+        boolean isSummer = false;
+        boolean isAutumn = true;
+
+        String name = "Darja";
+        System.out.println(name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
+
